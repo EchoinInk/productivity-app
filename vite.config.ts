41a35bbaa9f5abf457a50a9692be2734e88c6da2
EchoinInk <alex@echoin.ink>
2026-05-02 23:55:18 +1200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig(({ mode }) => {
   const isDev = mode === "development";
 
   return {
+    base: "/app/",
     plugins: [
       react(),
       isDev && componentTagger()
